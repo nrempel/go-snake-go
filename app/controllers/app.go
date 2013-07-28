6a31246eec1411488718cc03282ba52109970fcb
Nicholas Rempel <nbrempel@gmail.com>
2013-07-28 15:18:23 -0700

    stuff

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,7 +1,13 @@
 package controllers
 
 import "github.com/robfig/revel"
+import "encoding/json"
 
 type App struct {
 	*revel.Controller
+}
+
+func (c App) Register() revel.Result {
+	b, err := ioutil.ReadAll(c.Request.Body)
+	revel.INFO.Println(string(b), err)
 }
